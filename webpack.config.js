78e520a9255303f04bcb1d3144f5dec51ad3a3f0
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-07-24 10:19:57 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',  
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+],
+    mode: 'development', 
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+};
