4a9179b692af2e11957ff5b2150854aac1967821
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-07-25 16:35:58 -0400

    Update to class based syntax

--- src/index.js
+++ src/index.js
@@ -32,14 +32,18 @@ class WeatherApp {
   constructor(element) {
     this.element = element;
     this.dataElement = document.querySelector("#data");
-    this.cityInput = document.querySelector("#searchbar");
-    this.searchButton = document.querySelector("#search");
+    this.cityInput = document.querySelector("#searchField");
+    this.searchButton = document.querySelector("#search-button");
+    this.cityDiv = document.querySelector(".city-info");
+    this.mainInfoDiv = document.querySelector(".main-info");
+    this.secondaryInfoDiv = document.querySelector(".secondary-info");
+    this.forecastDiv = document.querySelector(".forecast");
 
     this.element.addEventListener("keydown", (e) => {
       this.handleKeyDown(e);
     });
     this.searchButton.addEventListener("click", (e) => {
-      this.clearCityData();
+      userInterface.clearCityData();
       this.getWeather();
     });
   }
@@ -47,7 +51,7 @@ class WeatherApp {
   async getWeather() {
     // let cityValue = this.cityInput.value;
     try {
-      console.log(process.env);
+      console.log(this);
       const response = await fetch(
         `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${this.cityInput.value}?key=${process.env.API_KEY}`
       );
@@ -55,29 +59,40 @@ class WeatherApp {
       if (response.status === 200) {
         let weatherData = await response.json();
         console.log(weatherData, typeof weatherData);
-        this.dataElement.insertAdjacentHTML("beforeend", `${weatherData.resolvedAddress.toUpperCase()}: ${JSON.stringify(weatherData.currentConditions.temp)}`);
+        userInterface.updateUI(weatherData);
       } else {
-        throw new Error(await response.json()); 
+        throw new Error(await response.json());
       }
     } catch (err) {
-        console.log(err); 
-        // call UI update
-        return; 
+      console.log(err);
+      // call UI update
+      return;
     }
   }
 
   handleKeyDown(e) {
     if (e.key === "Enter" && this.cityInput.value.length >= 3) {
-      this.clearCityData();
+      userInterface.clearCityData();
       this.getWeather();
     } else {
       return;
     }
   }
+}
+
+class UI {
+  constructor(app) {
+    this.app = app;
+  }
+
+  updateUI(data) {
+    this.app.dataElement.insertAdjacentHTML("beforeend",`${data.resolvedAddress.toUpperCase()}: ${JSON.stringify(data.currentConditions.temp)}`);
+  }
 
   clearCityData() {
-    this.dataElement.innerHTML = "";
+    this.app.dataElement.innerHTML = "";
   }
 }
 
 const app = new WeatherApp(document.querySelector("#container"));
+const userInterface = new UI(app);
